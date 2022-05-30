b637575b2ee6d9975b58f860da069d2cd25b8b1c
BonanzaBegin <3403944365@qq.com>
2022-05-30 13:18:29 +0800

    添加TestClass

---
+++ UIControls/ExSimpleIoc/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExSimpleIoc
+{
+   public class TestClass
+    {
+        /*
+         * 
+         * 这是在hot-fix中添加的TestClass
+         * 
+         */
+    }
+
+}
